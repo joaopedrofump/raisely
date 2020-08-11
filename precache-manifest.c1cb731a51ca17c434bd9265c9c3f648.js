f89ba15a8b4336592bc5f304825ec9b70e1f86d4
Joao Furriel <joaofurriel@Joaos-MacBook-Pro.local>
2020-08-11 23:09:29 +0100

    Updates

---
+++ precache-manifest.c1cb731a51ca17c434bd9265c9c3f648.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06fe7a5f4b395ca8cc37af87113ae6d",
+    "url": "/raisely/index.html"
+  },
+  {
+    "revision": "b8cd771e27b285de4caf",
+    "url": "/raisely/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd52f06b7739885aaf30",
+    "url": "/raisely/static/js/2.af8c5e3c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/raisely/static/js/2.af8c5e3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8cd771e27b285de4caf",
+    "url": "/raisely/static/js/main.f2026473.chunk.js"
+  },
+  {
+    "revision": "ca9b96a3969ceb777f39",
+    "url": "/raisely/static/js/runtime-main.22ff009a.js"
+  },
+  {
+    "revision": "103537d5b159b1f997b15afd07511e37",
+    "url": "/raisely/static/media/background.103537d5.jpg"
+  }
+]);
